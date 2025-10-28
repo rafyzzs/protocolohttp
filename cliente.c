d6196c825b4ecc966f7cd20ecc55d646810cc54d
root <root@rafy.localdomain>
2025-10-28 09:33:42 -0300

    Bibliotecas

--- cliente.c
+++ cliente.c
@@ -1,5 +1,10 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>     
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>  
+#include <netdb.h>      
+#include <errno.h>      
+#include <libgen.h>    
