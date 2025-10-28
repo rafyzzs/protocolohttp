e33338972c62a9d87d57d7238d98ec6ebe0d249e
root <root@rafy.localdomain>
2025-10-28 11:21:53 -0300

    Bibliotecas Servidor

--- servidor.c
+++ servidor.c
@@ -1,5 +1,9 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>     
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <sys/stat.h>   
+#include <dirent.h>     
+#include <errno.h>      
